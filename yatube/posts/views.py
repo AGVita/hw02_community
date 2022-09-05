2056c053ef7d69768dd38ab08321711f132984db
Raymon Zarquon <rzarquon@vo-e1.msk.21-school.ru>
2022-09-05 18:44:54 +0300

    Changes in html, views, admin, models

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,26 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.conf import settings
 
 from .models import Post, Group
 
-POST_COUNT = 10
-
 
 def index(request):
-    posts = Post.objects.all()[:POST_COUNT]
-    template = 'posts/index.html'
+    posts = Post.objects.all()[:settings.POST_COUNT]
     context = {
         'posts': posts
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:POST_COUNT]
+    posts = group.posts.all()[:settings.POST_COUNT]
     context = {
         'group': group,
         'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
